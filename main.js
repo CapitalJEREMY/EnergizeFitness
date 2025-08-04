db17cb0ce75d17cc55e9269951170ef46c83dd9d
Jeremiah <jeremiahadesola@gmail.com>
2025-08-04 19:05:20 +0100

    initial changes

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const mobileMenu = document.getElementById("mobileMenu");
+    const closeMenu = document.getElementById("closeMenu");
+  
+    hamburger.addEventListener("click", () => {
+      mobileMenu.classList.add("show");
+    });
+  
+    closeMenu.addEventListener("click", () => {
+      mobileMenu.classList.remove("show");
+    });
+  });
+document.querySelectorAll('.faq-item').forEach(item => {
+    item.addEventListener('click', () => {
+      item.classList.toggle('active');
+    });
+});
